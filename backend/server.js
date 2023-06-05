213bb223b2a698bd455b0b9ab73dabbfd7da1e6b
ayano_kondo@mail.toyota.co.jp <aya-no>
2023-06-05 11:15:05 +0900

    修正

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
   // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://smugshoppingapi.onrender.com"
+    "https://smugshopping.onrender.com"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "*");
